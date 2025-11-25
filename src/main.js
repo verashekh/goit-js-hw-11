a89703d91b17b93e8a42e78628d324a3fd6c7d57
Vera <verashekh@gmail.com>
2025-11-25 13:53:24 +0200

    modify all

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
